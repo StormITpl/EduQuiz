57aaa1c06c2f308c6938857dca595a857e7ba73f
Manes79 <mszornel@gmail.com>
2023-01-17 21:05:23 +0100

    final version

--- src/main/resources/db/version/dev/001-create-dev-table.sql
+++
@@ -1,7 +0,0 @@
-
-create table if not exists quizUser
-(
-    id          uuid not null primary key,
-    name        varchar(100) not null,
-    secondName  varchar(100) not null
-)
--- src/main/resources/db/version/dev/001-insert-into-dev-table.sql
+++
@@ -1,2 +0,0 @@
-insert into quizUser (id, name, secondName) values
-                                              (random_uuid(), '', '');
