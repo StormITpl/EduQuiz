27e5a97ca112c52d457b6a3af22ee361291f04cd
Jacek <steiner@10g.pl>
2023-01-25 23:40:41 +0100

    gamerepository inactive

--- src/main/java/pl/stormit/eduquiz/game/domain/repository/GameRepository.java
+++ src/main/java/pl/stormit/eduquiz/game/domain/repository/GameRepository.java
@@ -1,11 +1,11 @@
-package pl.stormit.eduquiz.game.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.stormit.eduquiz.game.domain.entity.Game;
-
-import java.util.UUID;
-
-@Repository
-public interface GameRepository extends JpaRepository<UUID, Game> {
-}
+//package pl.stormit.eduquiz.game.domain.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import pl.stormit.eduquiz.game.domain.entity.Game;
+//
+//import java.util.UUID;
+//
+//@Repository
+//public interface GameRepository extends JpaRepository<UUID, Game> {
+//}
