bcbcb767dc0085fb1665d212844c844dc0fa9407
k4t4u <115025099+k4t4u@users.noreply.github.com>
2023-02-18 14:12:30 +0100

    feature: implemented method createGameService

--- src/main/java/pl/stormit/eduquiz/game/service/GameService.java
+++ src/main/java/pl/stormit/eduquiz/game/service/GameService.java
@@ -2,17 +2,30 @@ package pl.stormit.eduquiz.game.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.stormit.eduquiz.game.domain.entity.Game;
+import pl.stormit.eduquiz.game.domain.repository.GameRepository;
 import pl.stormit.eduquiz.game.dto.GameDto;
-import pl.stormit.eduquiz.quizcreator.quiz.dto.QuizDto;
-
-import java.util.List;
-import java.util.UUID;
+import pl.stormit.eduquiz.game.dto.GameMapper;
+import pl.stormit.eduquiz.quizcreator.domain.quiz.Quiz;
+import pl.stormit.eduquiz.quizcreator.domain.quiz.QuizRepository;
+import pl.stormit.eduquiz.quizcreator.domain.quiz.dto.QuizDto;
 
 @Service
 @RequiredArgsConstructor
 public class GameService {
 
+    private final QuizRepository quizRepository;
+
+    private final GameRepository gameRepository;
+
+    private final GameMapper gameMapper;
+
     public GameDto createGame(QuizDto quizRequest) {
-        return new GameDto(UUID.randomUUID(), List.of(UUID.randomUUID()));
+        Quiz quiz = quizRepository.findById(quizRequest.id()).orElseThrow(() -> {
+            throw new RuntimeException("The quiz does not exist");
+        });
+        Game game = new Game(quiz);
+
+        return gameMapper.mapGameEntityToGameDto(gameRepository.save(game));
     }
 }
