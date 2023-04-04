3330c592550b5e9f7022e7595138845811408d7a
RobertoJavaDev <robertojavadev@gmail.com>
2023-04-04 09:04:22 +0200

    feat: Added service tests and AnswerRequestDto

--- src/main/java/pl/stormit/eduquiz/quizcreator/domain/answer/dto/AnswerDto.java
+++ src/main/java/pl/stormit/eduquiz/quizcreator/domain/answer/dto/AnswerDto.java
@@ -1,7 +1,11 @@
 package pl.stormit.eduquiz.quizcreator.domain.answer.dto;
 
+import pl.stormit.eduquiz.quizcreator.domain.question.Question;
+
 import java.util.UUID;
 
-public record AnswerDto(UUID id, String content,
-                        boolean isCorrect) {
+public record AnswerDto(UUID id,
+                        String content,
+                        boolean isCorrect,
+                        Question question) {
 }
