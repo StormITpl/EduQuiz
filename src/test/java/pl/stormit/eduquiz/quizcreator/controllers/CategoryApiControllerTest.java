5e362dd7a4731df46afd29b12b09fb92c9f2d3c3
Piotr Jagodzinski <piotrjagodzinski@smartoakprojects.com>
2023-04-29 13:24:55 +0200

    refactoring test sector import*

--- src/test/java/pl/stormit/eduquiz/quizcreator/controllers/CategoryApiControllerTest.java
+++ src/test/java/pl/stormit/eduquiz/quizcreator/controllers/CategoryApiControllerTest.java
@@ -21,7 +21,10 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
