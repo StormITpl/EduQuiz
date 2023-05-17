177a20d5deba7da7a8c4b85f85dade99dce739fb
RobertoJavaDev <robertojavadev@gmail.com>
2023-05-17 09:24:18 +0200

    feat: Added view for available quizzes

--- src/main/java/pl/stormit/eduquiz/common/controller/IndexViewController.java
+++ src/main/java/pl/stormit/eduquiz/common/controller/IndexViewController.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.stormit.eduquiz.quizcreator.domain.category.CategoryService;
+import pl.stormit.eduquiz.quizcreator.domain.quiz.QuizService;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/")
@@ -14,10 +18,21 @@ public class IndexViewController {
 
     private final CategoryService categoryService;
 
+    private final QuizService quizService;
+
     @GetMapping
     public String indexView(Model model){
 
         model.addAttribute("categories", categoryService.getCategories());
+        model.addAttribute("quizzes", quizService.getQuizzes());
+
+        return "index";
+    }
+
+    @GetMapping("/category/{id}")
+    public String viewQuiz(@PathVariable UUID id, Model model){
+
+        model.addAttribute("quizzes", quizService.getQuizzes());
 
         return "index";
     }
