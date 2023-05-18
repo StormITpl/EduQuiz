5ccb7241442649c1033b4a078f9f91d60a8a7fb4
RobertoJavaDev <robertojavadev@gmail.com>
2023-05-18 08:49:44 +0200

    feat: Implemented html to show quiz

--- src/main/java/pl/stormit/eduquiz/common/controller/IndexViewController.java
+++ src/main/java/pl/stormit/eduquiz/common/controller/IndexViewController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.stormit.eduquiz.quizcreator.domain.answer.AnswerService;
 import pl.stormit.eduquiz.quizcreator.domain.category.CategoryService;
+import pl.stormit.eduquiz.quizcreator.domain.question.QuestionService;
 import pl.stormit.eduquiz.quizcreator.domain.quiz.QuizService;
 
 import java.util.UUID;
@@ -20,6 +22,10 @@ public class IndexViewController {
 
     private final QuizService quizService;
 
+    private final QuestionService questionService;
+
+    private final AnswerService answerService;
+
     @GetMapping
     public String indexView(Model model){
 
@@ -29,11 +35,30 @@ public class IndexViewController {
         return "index";
     }
 
+    @GetMapping("/quizzes")
+    public String showQuizzes(Model model){
+
+        model.addAttribute("categories", categoryService.getCategories());
+        model.addAttribute("quizzes", quizService.getQuizzes());
+
+        return "showQuizzes";
+    }
+
+
     @GetMapping("/category/{id}")
     public String viewQuiz(@PathVariable UUID id, Model model){
 
         model.addAttribute("quizzes", quizService.getQuizzes());
 
         return "index";
+    }
+
+    @GetMapping("quiz/{id}")
+    public String quiz(@PathVariable UUID id, Model model){
+
+        model.addAttribute("quiz", quizService.getQuiz(id));
+        model.addAttribute("question", quizService.getQuiz(id).questions());
+
+        return "quiz";
     }
 }
