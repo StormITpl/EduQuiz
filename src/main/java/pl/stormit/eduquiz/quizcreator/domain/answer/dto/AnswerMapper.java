78302591bd05cf390e156556bcf4979fbf415d81
Jacek <steiner@10g.pl>
2023-05-22 21:54:08 +0200

    Answer module fixed

--- src/main/java/pl/stormit/eduquiz/quizcreator/domain/answer/dto/AnswerMapper.java
+++ src/main/java/pl/stormit/eduquiz/quizcreator/domain/answer/dto/AnswerMapper.java
@@ -3,12 +3,17 @@ package pl.stormit.eduquiz.quizcreator.domain.answer.dto;
 import org.mapstruct.Mapper;
 import pl.stormit.eduquiz.quizcreator.domain.answer.Answer;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface AnswerMapper {
+//    AnswerDto mapAnswerEntityToAnswerDto(Answer answer);
 
-    AnswerDto mapAnswerEntityToAnswerDto(Answer answer);
-
-    List<AnswerDto> mapAnswerEntityToAnswerDtoList(Iterable<Answer> answerList);
+    default AnswerDto mapAnswerEntityToAnswerDto(Answer answer){
+        return AnswerDto
+                .builder()
+                .id(answer.getId())
+                .content(answer.getContent())
+                .isCorrect(answer.isCorrect())
+                .question(answer.getQuestion())
+                .build();
+    }
 }
--- src/main/java/pl/stormit/eduquiz/quizcreator/domain/answer/dto/AnswerRequestDto.java
+++ src/main/java/pl/stormit/eduquiz/quizcreator/domain/answer/dto/AnswerRequestDto.java
@@ -1,7 +1,9 @@
 package pl.stormit.eduquiz.quizcreator.domain.answer.dto;
 
+import lombok.Builder;
 import pl.stormit.eduquiz.quizcreator.domain.question.Question;
 
+@Builder
 public record AnswerRequestDto(String content,
                                boolean isCorrect,
                                Question question) {
