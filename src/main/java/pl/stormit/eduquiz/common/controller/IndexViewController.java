8f1b563ef36d413904e9577847e18b175caecdcc
MichalMarciniakGS <72413371+MichalMarciniakGS@users.noreply.github.com>
2023-05-31 22:16:44 +0200

    Changes in Controller and changes for quiz.html

--- src/main/java/pl/stormit/eduquiz/common/controller/IndexViewController.java
+++ src/main/java/pl/stormit/eduquiz/common/controller/IndexViewController.java
@@ -3,14 +3,16 @@ package pl.stormit.eduquiz.common.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import pl.stormit.eduquiz.quizcreator.domain.answer.Answer;
 import pl.stormit.eduquiz.quizcreator.domain.answer.AnswerService;
 import pl.stormit.eduquiz.quizcreator.domain.category.CategoryService;
+import pl.stormit.eduquiz.quizcreator.domain.question.Question;
 import pl.stormit.eduquiz.quizcreator.domain.question.QuestionService;
 import pl.stormit.eduquiz.quizcreator.domain.quiz.QuizService;
+import pl.stormit.eduquiz.quizcreator.domain.quiz.dto.QuizDto;
 
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -53,12 +55,67 @@ public class IndexViewController {
         return "index";
     }
 
-    @GetMapping("quiz/{id}")
+    @GetMapping("/quiz/{id}")
     public String quiz(@PathVariable UUID id, Model model){
 
-        model.addAttribute("quiz", quizService.getQuiz(id));
-        model.addAttribute("questions", quizService.getQuiz(id).questions());
+        QuizDto quiz = quizService.getQuiz(id);
+        List<Question> questions = quiz.questions();
+
+        Integer questionIndex = (Integer) model.getAttribute("questionIndex");
+
+        if (questionIndex == null) {
+            questionIndex = 0;
+        }
+
+
+        Question currentQuestion = questions.get(questionIndex);
+        List<Answer> currentAnswers = currentQuestion.getAnswers();
+
+        model.addAttribute("quiz", quiz);
+        model.addAttribute("question", currentQuestion);
+        model.addAttribute("answers", currentAnswers);
 
         return "quiz";
+    }
+
+    @PostMapping("/quiz/{id}/{questionIndex}")
+    public String nextQuestion(@PathVariable UUID id, @PathVariable Integer questionIndex, Model model) {
+        QuizDto quiz = quizService.getQuiz(id);
+        List<Question> questions = quiz.questions();
+
+        if (questionIndex + 1 < questions.size()) {
+            questionIndex++;
+        }
+
+        Question currentQuestion = questions.get(questionIndex);
+        List<Answer> currentAnswers = currentQuestion.getAnswers();
+
+        model.addAttribute("quiz", quiz);
+        model.addAttribute("question", currentQuestion);
+        model.addAttribute("answers", currentAnswers);
+        model.addAttribute("questionIndex", questionIndex);
+
+//        return "quiz";
+        return "redirect:/quiz/{id}/{questionIndex}";
+    }
+    @GetMapping("/quiz/{id}/{questionIndex}")
+    public String nextQuestionGet(@PathVariable UUID id, @PathVariable Integer questionIndex, Model model) {
+        QuizDto quiz = quizService.getQuiz(id);
+        List<Question> questions = quiz.questions();
+
+        if (questionIndex + 1 < questions.size()) {
+            questionIndex++;
+        }
+
+        Question currentQuestion = questions.get(questionIndex);
+        List<Answer> currentAnswers = currentQuestion.getAnswers();
+
+        model.addAttribute("quiz", quiz);
+        model.addAttribute("question", currentQuestion);
+        model.addAttribute("answers", currentAnswers);
+        model.addAttribute("questionIndex", questionIndex);
+
+        return "quiz";
+//        return "redirect:/quiz/{id}/{questionIndex}";
     }
 }
