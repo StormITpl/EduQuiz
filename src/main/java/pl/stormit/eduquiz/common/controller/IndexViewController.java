3a954f745d019a7dcaa345a0f04a95ca90a3a737
MichalMarciniakGS <72413371+MichalMarciniakGS@users.noreply.github.com>
2023-06-17 14:00:31 +0200

    Refactor - IndexViewController and quiz.html

--- src/main/java/pl/stormit/eduquiz/common/controller/IndexViewController.java
+++ src/main/java/pl/stormit/eduquiz/common/controller/IndexViewController.java
@@ -1,26 +1,23 @@
 package pl.stormit.eduquiz.common.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import pl.stormit.eduquiz.game.domain.entity.Game;
-import pl.stormit.eduquiz.game.domain.repository.GameRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.stormit.eduquiz.game.dto.GameDto;
-import pl.stormit.eduquiz.game.dto.GameMapper;
-import pl.stormit.eduquiz.game.dto.GameMapperImpl;
 import pl.stormit.eduquiz.game.service.GameService;
 import pl.stormit.eduquiz.quizcreator.domain.answer.Answer;
 import pl.stormit.eduquiz.quizcreator.domain.answer.AnswerService;
 import pl.stormit.eduquiz.quizcreator.domain.category.CategoryService;
 import pl.stormit.eduquiz.quizcreator.domain.question.Question;
-import pl.stormit.eduquiz.quizcreator.domain.question.QuestionService;
 import pl.stormit.eduquiz.quizcreator.domain.quiz.QuizService;
 import pl.stormit.eduquiz.quizcreator.domain.quiz.dto.QuizDto;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Controller
@@ -33,14 +30,11 @@ public class IndexViewController {
     private final QuizService quizService;
 
     private final GameService gameService;
-    private final GameRepository gameRepository;
-
-    private final QuestionService questionService;
 
     private final AnswerService answerService;
 
     @GetMapping
-    public String indexView(Model model){
+    public String indexView(Model model) {
 
         model.addAttribute("categories", categoryService.getCategories());
         model.addAttribute("quizzes", quizService.getQuizzes());
@@ -49,7 +43,7 @@ public class IndexViewController {
     }
 
     @GetMapping("/quizzes")
-    public String showQuizzes(Model model){
+    public String showQuizzes(Model model) {
 
         model.addAttribute("categories", categoryService.getCategories());
         model.addAttribute("quizzes", quizService.getQuizzes());
@@ -59,7 +53,7 @@ public class IndexViewController {
 
 
     @GetMapping("/category/{id}")
-    public String viewQuiz(@PathVariable UUID id, Model model){
+    public String viewQuiz(@PathVariable UUID id, Model model) {
 
         model.addAttribute("quizzes", quizService.getQuizzes());
 
@@ -67,24 +61,11 @@ public class IndexViewController {
     }
 
     @GetMapping("/quiz/{id}/{questionIndex}")
-    public String quiz(@PathVariable UUID id, @PathVariable int questionIndex, Model model){
+    public String quiz(@PathVariable UUID id, @PathVariable int questionIndex, Model model) {
 
         QuizDto quizDto = quizService.getQuiz(id);
         List<Question> questions = quizDto.questions();
-
-
         GameDto gameDto = gameService.createGame(quizDto);
-
-
-//        gameDto = gameService.createGame(quizDto);
-
-//        questionIndex = (Integer) model.getAttribute("questionIndex");
-
-//        if (questionIndex == null) {
-//            questionIndex = 0;
-//        }
-
-
 
         Question currentQuestion = questions.get(questionIndex);
         List<Answer> currentAnswers = currentQuestion.getAnswers();
@@ -94,9 +75,6 @@ public class IndexViewController {
         model.addAttribute("answers", currentAnswers);
         model.addAttribute("questionIndex", questionIndex);
         model.addAttribute("gameDtoId", gameDto.id());
-
-        System.out.println("gameDtoId z GETa " + gameDto.id());
-
 
         return "quiz";
     }
@@ -110,19 +88,13 @@ public class IndexViewController {
         QuizDto quiz = quizService.getQuiz(id);
         List<Question> questions = quiz.questions();
         UUID selectedAnswerId = UUID.fromString(selectedAnswerIdRequest);
-        System.out.println("gameDtoId z POSTA " + gameDtoId); //Test
-//        String substring = gameDtoId.substring(0, 36);
-//        System.out.println("substring z gameDtoID: " + substring); //Test
         UUID gameDtoUUID = UUID.fromString(gameDtoId);
-        System.out.println("gameDtoUUID: " + gameDtoUUID);
-        gameService.playGame(gameDtoUUID,answerService.getAnswer(selectedAnswerId));
-
+        gameService.playGame(gameDtoUUID, answerService.getAnswer(selectedAnswerId));
 
         questionIndex++;
 
         Question currentQuestion = questions.get(questionIndex);
         List<Answer> currentAnswers = currentQuestion.getAnswers();
-//        gameService.playGame(gameDto.id(),);
 
         model.addAttribute("quiz", quiz);
         model.addAttribute("question", currentQuestion);
@@ -130,37 +102,6 @@ public class IndexViewController {
         model.addAttribute("questionIndex", questionIndex);
         model.addAttribute("gameDtoId", gameDtoId);
 
-//        questionIndex++;
-//        String newUrl = "quiz/{id}/questionIndex?questionIndex=" + questionIndex;
-
-//        return "quiz";
-//        return "redirect:" +newUrl;
-        System.out.println("++++++++++++++++++++++++++++++");
-        System.out.println(questionIndex);
-//        return "redirect:/quiz/{id}/{questionIndex}";
         return "quiz";
     }
-//    @GetMapping("/quiz/{id}/{questionIndex}")
-//    public String nextQuestionGet(@PathVariable UUID id, @PathVariable Integer questionIndex, Model model) {
-//        QuizDto quiz = quizService.getQuiz(id);
-//        List<Question> questions = quiz.questions();
-//
-//        if (questionIndex + 1 < questions.size()) {
-//            questionIndex++;
-//        }
-//
-//        Question currentQuestion = questions.get(questionIndex);
-//        List<Answer> currentAnswers = currentQuestion.getAnswers();
-//
-//        model.addAttribute("quiz", quiz);
-//        model.addAttribute("question", currentQuestion);
-//        model.addAttribute("answers", currentAnswers);
-//        model.addAttribute("questionIndex", questionIndex);
-//
-//        return "quiz";
-////        questionIndex++;
-////        String newUrl = "quiz/{id}/questionIndex?questionIndex=" + questionIndex;
-////        return "redirect:" +newUrl;
-////        return "redirect:/quiz/{id}/{questionIndex}";
-//    }
 }
