54acedd734cc9c124c79cc5069214293b427e418
Piotr Jagodzinski <piotrjagodzinski@smartoakprojects.com>
2023-09-20 17:57:05 +0200

    Task 58 unstack imports

--- src/main/java/pl/stormit/eduquiz/quizcreator/domain/user/User.java
+++ src/main/java/pl/stormit/eduquiz/quizcreator/domain/user/User.java
@@ -1,6 +1,17 @@
 package pl.stormit.eduquiz.quizcreator.domain.user;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
