7e234b0aa94d6db902419f562c4fc1b528adcc4c
MichalMarciniakGS <72413371+MichalMarciniakGS@users.noreply.github.com>
2023-09-20 22:18:53 +0200

    Modified SecurityConfig (in progress) and added CustomUserDetailService class (in progress)

--- src/main/java/pl/stormit/eduquiz/authenticator/SecurityConfig.java
+++ src/main/java/pl/stormit/eduquiz/authenticator/SecurityConfig.java
@@ -1,9 +1,15 @@
 package pl.stormit.eduquiz.authenticator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,7 +25,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/categoryManagement").hasRole("ADMIN")
-                        .anyRequest().permitAll());
+                        .anyRequest().permitAll())
+                .formLogin()
+                .and()
+                .logout();
+
 
         return http.build();
     }
