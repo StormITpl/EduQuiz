5770d477dd2dc5f46a93beb17bd8d2b042355900
MichalMarciniakGS <72413371+MichalMarciniakGS@users.noreply.github.com>
2023-09-20 21:31:53 +0200

    Added Spring Security dependency and basic SecurityConfig

---
+++ src/main/java/pl/stormit/eduquiz/authenticator/SecurityConfig.java
@@ -0,0 +1,28 @@
+package pl.stormit.eduquiz.authenticator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .cors()
+                .disable();
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/categoryManagement").hasRole("ADMIN")
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+}
