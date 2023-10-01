738b8cbe79f70909611e0c258ee9a792ea9c7301
Slawek <slapyt84@gmail.com>
2023-10-01 17:56:04 +0200

    Registration Update. Prepared for validation.

--- src/main/java/pl/stormit/eduquiz/authenticator/controllers/AuthenticationViewController.java
+++ src/main/java/pl/stormit/eduquiz/authenticator/controllers/AuthenticationViewController.java
@@ -1,10 +1,18 @@
 package pl.stormit.eduquiz.authenticator.controllers;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.stormit.eduquiz.quizcreator.domain.user.UserRepository;
 import pl.stormit.eduquiz.quizcreator.domain.user.UserService;
+import pl.stormit.eduquiz.quizcreator.domain.user.dto.UserDto;
+import pl.stormit.eduquiz.quizcreator.domain.user.dto.UserRequestDto;
 
 @Controller
 @RequestMapping("/register")
@@ -13,16 +21,33 @@ public class AuthenticationViewController {
 
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @GetMapping()
-    public String registerUser() {
-//        UserDto registeredUser = userService.createUser(user);
+    public String registerUser(final Model model, Error error) {
+        model.addAttribute("userRequestDto", new UserRequestDto(null, null));
+        model.addAttribute("error", error);
         return "register";
     }
 
-//    @PostMapping()
-//    public String registerUser(@RequestBody UserRequestDto user){
-//        UserDto registeredUser = userService.createUser(user);
-//        return "register";
-//    }
+    @PostMapping()
+    public String registerUser(@Valid UserRequestDto userRequestDto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("registrationForm", userRequestDto);
+            return "register";
+        }
+
+        if (userRepository.findByNickname(userRequestDto.nickname()).isPresent()) {
+            bindingResult.rejectValue("nickname",
+                    "userRequestDto.nickname",
+                    "An account already exists for this nickname.");
+            return "register";
+        }
+
+        UserDto registeredUser = userService.createUser(userRequestDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("message", "User was successfully registered");
+        System.out.println(registeredUser);
+        return "redirect:/register";
+    }
 }
