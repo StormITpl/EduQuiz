f57662954aa76762eab61fafa868982ea9492e20
RobertoJavaDev <robertojavadev@gmail.com>
2023-11-03 20:16:31 +0100

    feat: Implemented user admin controller

--- src/main/java/pl/stormit/eduquiz/admin/controllers/UserAdminViewController.java
+++ src/main/java/pl/stormit/eduquiz/admin/controllers/UserAdminViewController.java
@@ -2,6 +2,8 @@ package pl.stormit.eduquiz.admin.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.stormit.eduquiz.quizcreator.domain.user.UserService;
 
@@ -11,4 +13,12 @@ import pl.stormit.eduquiz.quizcreator.domain.user.UserService;
 public class UserAdminViewController {
 
     private final UserService userService;
+
+    @GetMapping
+    public String viewUsers(Model model) {
+
+        model.addAttribute("users", userService.getUsers());
+
+        return "viewUsers";
+    }
 }
