9cbbda9aa077bdc9ff8c1047f9241cbb0c9c1b5a
Slawek <slapyt84@gmail.com>
2023-11-16 07:33:45 +0100

    Added required query

--- src/main/java/pl/stormit/eduquiz/game/domain/repository/GameRepository.java
+++ src/main/java/pl/stormit/eduquiz/game/domain/repository/GameRepository.java
@@ -4,10 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.stormit.eduquiz.game.domain.entity.Game;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, UUID> {
+
     Optional<Game> findTopByQuiz_IdOrderByDurationAsc(UUID quizId);
+
+    Long countAllByQuiz_Id(UUID quizId);
+
+    List<Game> findGameByStartBetween(LocalDateTime start, LocalDateTime finish);
+
 }
