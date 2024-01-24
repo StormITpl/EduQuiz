59a016d55540a7fe7b9696d0d6cd81bc6937eabd
Slawek <slapyt84@gmail.com>
2024-01-24 22:51:48 +0100

    Added query

--- src/main/java/pl/stormit/eduquiz/quizcreator/domain/quiz/QuizRepository.java
+++ src/main/java/pl/stormit/eduquiz/quizcreator/domain/quiz/QuizRepository.java
@@ -1,6 +1,7 @@
 package pl.stormit.eduquiz.quizcreator.domain.quiz;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.UUID;
 public interface QuizRepository extends JpaRepository<Quiz, UUID> {
 
     List<Quiz> getAllByCategoryId(UUID id);
+
+    @Query("select q from Quiz q order by q.createdAt desc limit 3")
+    List<Quiz> getThreeNewest();
 }
