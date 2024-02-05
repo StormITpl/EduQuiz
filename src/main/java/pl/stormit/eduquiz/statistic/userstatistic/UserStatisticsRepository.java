43df837e063227eadb160f6f0d1cc42b6ff78d5c
RobertoJavaDev <robertojavadev@gmail.com>
2024-02-05 19:15:19 +0100

    refactor: Changed access modifier

--- src/main/java/pl/stormit/eduquiz/statistic/userstatistic/UserStatisticsRepository.java
+++ src/main/java/pl/stormit/eduquiz/statistic/userstatistic/UserStatisticsRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface UserStatisticsRepository extends JpaRepository<UserStatistics, UUID> {
+interface UserStatisticsRepository extends JpaRepository<UserStatistics, UUID> {
 }
