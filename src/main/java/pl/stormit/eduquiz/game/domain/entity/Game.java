70cc7bffeab493b6a866d17aa4615b2e68e15d49
Slawek <slapyt84@gmail.com>
2024-02-06 10:01:03 +0100

    Added created_at

--- src/main/java/pl/stormit/eduquiz/game/domain/entity/Game.java
+++ src/main/java/pl/stormit/eduquiz/game/domain/entity/Game.java
@@ -1,17 +1,20 @@
 package pl.stormit.eduquiz.game.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import pl.stormit.eduquiz.quizcreator.domain.quiz.Quiz;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -34,5 +37,13 @@ public class Game {
 
     public Game(Quiz quiz) {
         this.quiz = quiz;
+    }
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void setCreatedAt() {
+        this.createdAt = LocalDateTime.now();
     }
 }
