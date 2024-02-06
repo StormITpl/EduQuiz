9c292fb1d4572ce334a615ec5d904744130b4f9d
Slawek <slapyt84@gmail.com>
2024-02-06 08:38:05 +0100

    Added new method add

--- src/main/java/pl/stormit/eduquiz/statistic/quizstatistic/QuizStatisticFacade.java
+++ src/main/java/pl/stormit/eduquiz/statistic/quizstatistic/QuizStatisticFacade.java
@@ -1,5 +1,6 @@
 package pl.stormit.eduquiz.statistic.quizstatistic;
 
+import pl.stormit.eduquiz.game.domain.entity.Game;
 import pl.stormit.eduquiz.quizcreator.domain.quiz.Quiz;
 
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.List;
 public interface QuizStatisticFacade {
 
     List<Quiz> getThreeNewest();
+
+    QuizStatistic add(Game game, int score, long duration);
 }
--- src/main/java/pl/stormit/eduquiz/statistic/quizstatistic/QuizStatisticFacadeImp.java
+++ src/main/java/pl/stormit/eduquiz/statistic/quizstatistic/QuizStatisticFacadeImp.java
@@ -1,18 +1,29 @@
 package pl.stormit.eduquiz.statistic.quizstatistic;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.stormit.eduquiz.game.domain.entity.Game;
 import pl.stormit.eduquiz.quizcreator.domain.quiz.Quiz;
 import pl.stormit.eduquiz.quizcreator.domain.quiz.QuizService;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@Component
 public class QuizStatisticFacadeImp implements QuizStatisticFacade{
 
     private final QuizService quizService;
+
+    private final QuizStatisticService statisticService;
 
     @Override
     public List<Quiz> getThreeNewest() {
         return quizService.getThreeNewest();
     }
+
+    @Override
+    public QuizStatistic add(Game game, int score, long duration) {
+        return statisticService.add(game, score, duration);
+    }
+
 }
