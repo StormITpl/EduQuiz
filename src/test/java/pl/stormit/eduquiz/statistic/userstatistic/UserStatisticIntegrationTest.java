43ad6553fffd24d66f749aae37d256fe23ff53f8
Piotr Jagodzinski <piotrjagodzinski@smartoakprojects.com>
2024-02-16 21:33:08 +0100

    Task 80 - @ActiveProfiles({"test"})

--- src/test/java/pl/stormit/eduquiz/statistic/userstatistic/UserStatisticIntegrationTest.java
+++ src/test/java/pl/stormit/eduquiz/statistic/userstatistic/UserStatisticIntegrationTest.java
@@ -3,10 +3,12 @@ package pl.stormit.eduquiz.statistic.userstatistic;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import pl.stormit.eduquiz.quizcreator.domain.user.UserService;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ActiveProfiles({"test"})
 @SpringBootTest
 class UserStatisticIntegrationTest {
 
