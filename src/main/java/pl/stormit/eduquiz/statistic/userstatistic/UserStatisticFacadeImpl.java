b607ee4be22a99cb22e6899cebe8174aef8b9967
RobertoJavaDev <robertojavadev@gmail.com>
2024-03-11 08:29:23 +0100

    feat: Impleented lastLoginByUser method

--- src/main/java/pl/stormit/eduquiz/statistic/userstatistic/UserStatisticFacadeImpl.java
+++ src/main/java/pl/stormit/eduquiz/statistic/userstatistic/UserStatisticFacadeImpl.java
@@ -1,16 +1,31 @@
 package pl.stormit.eduquiz.statistic.userstatistic;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import pl.stormit.eduquiz.quizcreator.domain.user.UserService;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Validated
 public class UserStatisticFacadeImpl implements UserStatisticFacade {
     private final UserService userService;
+
+    private final UserStatisticRepository userStatisticRepository;
 
     @Override
     public long getTotalNumberOfUsers() {
         return userService.getTotalNumberOfUsers();
+    }
+
+    @Override
+    public Instant lastLoginByUser(UUID userId) {
+        UserStatistic userStatistics = userStatisticRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("The user statistics by id: " + userId + ", does not exist."));
+        return userStatistics.getLastLoginDate();
     }
 }
