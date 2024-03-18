e4ee0ba92a840f4e59b605416c9bd1c1cdc30439
MichalMarciniakGS <72413371+MichalMarciniakGS@users.noreply.github.com>
2024-03-18 19:15:48 +0100

    Added new Test for Lowest Score Statistic

--- src/test/java/pl/stormit/eduquiz/statistic/quizstatistic/QuizStatisticFacadeImpIntegrationTestAdditional.java
+++ src/test/java/pl/stormit/eduquiz/statistic/quizstatistic/QuizStatisticFacadeImpIntegrationTestAdditional.java
@@ -46,21 +46,33 @@ class QuizStatisticFacadeImpIntegrationTestAdditional {
 //    private QuizService quizService;
 
     private  int score;
+    private  int score2;
 //    private  final String USER_NAME = "user";
 //    private  final UUID USER_UUID = UUID.fromString("f825606e-c660-4675-9a3a-b19e77c82501");
     private  final UUID STAT_UUID = UUID.fromString("f825606e-c660-4675-9a3a-b19e77c82505");
+    private  final UUID STAT_UUID2 = UUID.fromString("f825606e-c660-4675-9a3a-b19e77c82333");
     private  final UUID GAME_UUID = UUID.fromString("f825606e-c660-4675-9a3a-b19e77c82508");
+    private  final UUID GAME_UUID2 = UUID.fromString("f825606e-c660-4675-9a3a-b19e77c82338");
     private  final Game game = new Game();
+    private  final Game game2 = new Game();
     private  final User firstUser = new User();
+    private  final User secondUser = new User();
     private  final QuizStatistic statistic = new QuizStatistic();
+    private  final QuizStatistic statistic2 = new QuizStatistic();
 
     @BeforeEach
     void SetUp(){
+
         score = 10;
+        score2 = 5;
 
         game.setId(GAME_UUID);
         game.setCreatedAt(LocalDateTime.now().minusSeconds(10));
 
+        game2.setId(GAME_UUID2);
+        game2.setCreatedAt(LocalDateTime.now().minusSeconds(15));
+
+        //First user set-up
         firstUser.setNickname("Ananiasz");
         firstUser.setEmail("ananiasz@gmail.com");
         firstUser.setPassword("Password123!");
@@ -68,11 +80,26 @@ class QuizStatisticFacadeImpIntegrationTestAdditional {
         firstUser.setRole(Role.ROLE_ADMIN);
         userRepository.save(firstUser);
 
+        //Second user set-up
+        secondUser.setNickname("Barabasz");
+        secondUser.setEmail("barabasz@gmail.com");
+        secondUser.setPassword("Password456!");
+        secondUser.setStatus(Status.VERIFIED);
+        secondUser.setRole(Role.ROLE_ADMIN);
+        userRepository.save(secondUser);
+
         statistic.setCreatedAt(LocalDateTime.now());
         statistic.setDuration(ChronoUnit.SECONDS.between(game.getCreatedAt(), LocalDateTime.now()));
         statistic.setScore(score);
+
         statistic.setUserId(firstUser.getId());
         statistic.setId(STAT_UUID);
+
+        statistic2.setCreatedAt(LocalDateTime.now());
+        statistic2.setDuration(ChronoUnit.SECONDS.between(game2.getCreatedAt(), LocalDateTime.now()));
+        statistic2.setScore(score2);
+        statistic2.setUserId(secondUser.getId());
+        statistic2.setId(STAT_UUID2);
 
     }
 
@@ -91,5 +118,29 @@ class QuizStatisticFacadeImpIntegrationTestAdditional {
         int highestScore = quizStatisticService.getHighestScore();
 
         assertEquals(10,highestScore);
+    }
+
+    @Test
+    void shouldReturnLowestScore(){
+        // given
+
+        userRepository.save(firstUser);
+        userRepository.save(secondUser);
+
+        gameRepository.save(game);
+        gameRepository.save(game2);
+        List<Game> all = gameRepository.findAll();
+        UUID gameRepId = all.get(0).getId();
+        UUID gameRepId2 = all.get(1).getId();
+        Optional<Game> byId = gameRepository.findById(gameRepId);
+        Optional<Game> byId2 = gameRepository.findById(gameRepId2);
+        statistic.setGame(byId.get());
+        quizStatisticRepository.save(statistic);
+        statistic2.setGame(byId2.get());
+        quizStatisticRepository.save(statistic2);
+
+        int lowestScore = quizStatisticService.getLowestScore();
+
+        assertEquals(5,lowestScore);
     }
 }
